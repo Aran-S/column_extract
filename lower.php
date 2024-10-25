7f83b1b2ac2dda09df681681441ab6d854860593
Aran <aranadthiya@gmail.com>
2024-10-25 21:50:57 +0530

    Refactor database connection credentials in lower.php

--- lower.php
+++ lower.php
@@ -3,7 +3,6 @@ $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = ""; /* Password */
 $dbname = "db_name"; /* Database name */
-//hfgfgdffgtd
 global $con;
 $con = mysqli_connect($host, $user, $password,$dbname);
 if (!$con) 
